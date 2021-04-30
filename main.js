0c301c2a481a8affa70dc88c473638c2248521c5
Dave McLaughlin <skyganteng1@gmail.com>
2021-04-30 21:33:13 +0800

    ds

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const existingConfig = fs.existsSync('info.json'); 
+const question = []
+
+function buildConfig(){
+    console.log("Harap tunggu...")
+}
+
+if(existingConfig){
+    inquirer
+    .prompt([
+        {
+            type: 'confirm',
+            name: 'overwrite',
+            message: 'info.json sudah tersedia! ingin membuatnya lagi?',
+            default: 'false'
+        }
+    ])
+    .then(answers =>{
+        if(answers.overwrite){
+            buildConfig();
+        } else (
+            console.log("Okeh, terima kasih! 👋")
+        )
+    })
+}
+
 console.log(`work in progress`);
